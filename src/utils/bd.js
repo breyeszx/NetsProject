2e1cf1fde82b12c472de6b8ad5b73ad8ffdbe39c
breyeszx <breyes1864@gmail.com>
2024-08-04 14:38:30 -0400

    db problems

--- src/utils/bd.js
+++ src/utils/bd.js
@@ -1,11 +1,11 @@
 // lib/dbConnect.js
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
+    "Please define the MONGODB_URI environment variable inside .env.local"
   );
 }
 
@@ -22,8 +22,6 @@ async function dbConnect() {
 
   if (!cached.promise) {
     const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       bufferCommands: false,
     };
 
